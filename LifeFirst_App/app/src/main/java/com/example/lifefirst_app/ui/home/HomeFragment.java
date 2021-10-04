80500eaa5b4963ebd18c34a9edcc4d5ae0583a0f
Aparna Juhi <54853999+AparnaJuhi@users.noreply.github.com>
2021-10-04 21:15:47 +0530

    Home page text done

--- LifeFirst_App/app/src/main/java/com/example/lifefirst_app/ui/home/HomeFragment.java
+++ LifeFirst_App/app/src/main/java/com/example/lifefirst_app/ui/home/HomeFragment.java
@@ -13,6 +13,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.lifefirst_app.R;
+import com.synnapps.carouselview.CarouselView;
 
 public class HomeFragment extends Fragment {
 
@@ -23,13 +24,16 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.title_one);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                textView.setText("Who are we");
             }
         });
         return root;
+
+
+
     }
 }
