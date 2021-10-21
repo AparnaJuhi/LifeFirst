b9b11d1e64bfb2b61b835b98931e4e877e9fe3e5
Aparna Juhi <54853999+AparnaJuhi@users.noreply.github.com>
2021-10-21 22:15:11 +0530

    Navigation issue solved
    Navigation issue was due to mobile navigation- Fragments were made and its now working

--- LifeFirst_App/app/src/main/java/com/example/lifefirst_app/homepage.java
+++ LifeFirst_App/app/src/main/java/com/example/lifefirst_app/homepage.java
@@ -10,6 +10,9 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,11 +20,12 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
 
 public class homepage extends AppCompatActivity {
-
     private AppBarConfiguration mAppBarConfiguration;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,17 +40,14 @@ public class homepage extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_services, R.id.nav_ourstory, R.id.nav_aboutus, R.id.nav_contactus_, R.id.nav_joinus)
+                R.id.nav_home, R.id.nav_services, R.id.nav_ourstory, R.id.nav_aboutus, R.id.nav_contactus, R.id.nav_joinus)
                 .setDrawerLayout(drawer)
                 .build();
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
@@ -58,16 +59,6 @@ public class homepage extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.homepage, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        int id=item.getItemId();
-        if(id==R.id.action_settings)
-            return true;
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     public boolean onSupportNavigateUp() {
