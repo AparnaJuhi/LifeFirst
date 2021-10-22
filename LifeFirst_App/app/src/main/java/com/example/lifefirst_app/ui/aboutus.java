0d3e76fea14e7851e70d8ffc2190a2411c343563
Aparna Juhi <54853999+AparnaJuhi@users.noreply.github.com>
2021-10-22 17:33:45 +0530

    AboutUs page done

--- LifeFirst_App/app/src/main/java/com/example/lifefirst_app/ui/aboutus.java
+++ LifeFirst_App/app/src/main/java/com/example/lifefirst_app/ui/aboutus.java
@@ -2,6 +2,8 @@ package com.example.lifefirst_app.ui;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,13 +13,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.lifefirst_app.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class aboutus extends Fragment {
 
     private AboutusViewModel mViewModel;
-
+    private ImageView mImageView;
     public static aboutus newInstance() {
         return new aboutus();
     }
@@ -25,7 +30,52 @@ public class aboutus extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.aboutus_fragment, container, false);
+
+        View root = inflater.inflate(R.layout.aboutus_fragment, container, false);
+        //first of all we declare root and then return root
+        ImageView insta = (ImageView) root.findViewById(R.id.insta);
+        insta.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/lifefirst_24/"));
+                startActivity(intent);
+            }
+        });
+        ImageView facebook = (ImageView) root.findViewById(R.id.fb);
+        facebook.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/LifeFirst24"));
+                startActivity(intent);
+            }
+        });
+        ImageView twitter = (ImageView) root.findViewById(R.id.twi);
+        twitter.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/LifeFirst_24"));
+                startActivity(intent);
+            }
+        });
+
+        ImageView linkedin = (ImageView) root.findViewById(R.id.lin);
+        linkedin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/life-first-megakit-930500210/"));
+                startActivity(intent);
+            }
+        });
+
+        return root;
     }
 
     @Override
